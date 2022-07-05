522baa9fc49afd24f279f1862736be1505891c1f
Jeongsang Kim <jeongsang.kim@greencar.co.kr>
2022-07-05 13:44:39 +0900

    feat: 프로젝트 최초 업로드

---
+++ PracticeLint/PracticeLint/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeLint
+//
+//  Created by G00332 on 5/7/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
